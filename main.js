85210917132675524c4fa951c92664202a297f29
KumariTPT <kristofer.kumari@tptlive.ee>
2023-12-04 14:27:58 +0200

    yargs kalkulaator

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
-import chalk from 'chalk';
-import boxen from 'boxen';
-
-function write(text) {
-    process.stdout.write(text);
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+const argv = yargs(hideBin(process.argv))
+  .option('value1', {
+  alias: 'v1',
+  describe: 'First operand',
+  demandOption: true,
+  type: 'number',
+  })
+  .option('value2', {
+  alias: 'v2',
+  describe: 'Second operand',
+  demandOption: true,
+  type: 'number',
+  })
+  .option('sign', {
+  alias: 's',
+  describe: 'Math operation: +, -, *, /',
+  demandOption: true,
+  choices: ['+', '-', '*', '/'],
+  })
+  .help()
+  .argv;
+const { value1, value2, sign } = argv;
+ 
+let result;
+ 
+switch (sign) {
+ 
+  case '+':
+  result = value1 + value2;
+  break;
+ 
+  case '-':
+  result = value1 - value2;
+  break;
+ 
+  case '*':
+  result = value1 * value2;
+  break;
+ 
+  case '/':
+  result = value1 / value2;
+  break;
+ 
+  default:
+  console.error('Invalid sign');
+  process.exit(1);
 }
-
-console.log(boxen(chalk.bgGreen.magenta('Hello world'), {borderStyle:'round'}));
- 
-console.log(boxen(chalk.bgBlue.magentaBright('Vau'),{padding:6, margin:2, borderstyle: "doubleSingle", width: 7, borderColor:'red'}));
- 
-console.log(boxen(chalk.bgRed.strikethrough('Tervist'), {borderStyle:'double', borderColor:'blue', borderStyle:'round', margin: 13}));
- 
-console.log(boxen(chalk.overline.bgCyanBright.magentaBright('Jah'), {borderStyle:'single', borderColor:'red', title: 'kromps', titleAlignment: 'center'}));
- 
-console.log(boxen(chalk.bgRedBright.overline('ei'), {title: 'eegrid', borderStyle:'arrow', borderColor:'yellow'}));
- 
-console.log(boxen(chalk.bgBlack.italic('Maitea'), {padding: 4, margin: 3, width: 12, float:'center', borderColor:'blue'}));
- 
-console.log(boxen(chalk.bgYellow.inverse('Msasedw2n'), {borderStyle:'classic', width: 10, borderColor: '#8B0000'}));
+console.log(`${value1} ${sign} ${value2} = ${result}`);
